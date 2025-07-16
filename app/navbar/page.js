537f6e9260c4d229358aca341c5dc3f591693e1f
Daniel Sebastian G. Quilatan <179365223+samoyedq@users.noreply.github.com>
2025-07-16 11:41:27 +0800

    Changes (2)

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -25,7 +25,7 @@ export default function NavbarPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div className=" bg-gray-50 dark:bg-gray-900">
       <header className="bg-gray-800 shadow">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-7">
